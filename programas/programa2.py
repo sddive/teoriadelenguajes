3935aafde3e0a3a6b4152df9bba552fe87c50763
Mauricio Ferreyra (VPN) <mauricio.ferreyra@venhapranuvem.com.br>
2018-04-18 12:29:25 -0300

    salida

--- programas/programa2.py
+++ programas/programa2.py
@@ -3,11 +3,12 @@ import sys
 
 
 
-fname = sys.argv[0]
+fname = sys.argv[1]
+sys.stdout = open(sys.argv[2], 'w')
 
 pattern = re.compile(r'\b(if) *.* +then|\b(while) *.* +do|\b(for) *.* +do|\b(repeat) *.* +until')
 
-matches = pattern.findall(open('../entradas/entrada1.txt','r').read())
+matches = pattern.findall(open(fname,'r').read())
 
 print('if '+ str(len([match for match in matches if match[0] == 'if'])))
 print('for '+ str(len([match for match in matches if match[2] == 'for'])))
