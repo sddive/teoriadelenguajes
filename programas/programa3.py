b6a07497319b029541559f92861e57de941b3924
Mauricio Ferreyra (VPN) <mauricio.ferreyra@venhapranuvem.com.br>
2018-04-18 12:29:40 -0300

    salida

--- programas/programa3.py
+++ programas/programa3.py
@@ -1,11 +1,12 @@
 import re
 import sys
 
-fname = sys.argv[0]
+fname = sys.argv[1]
+sys.stdout = open(sys.argv[2], 'w')
 
 pattern = re.compile(r'^\s*(.*):=.*',re.M)
 
-matches = pattern.findall(open('../entradas/entrada3.txt','r').read())
+matches = pattern.findall(open(fname,'r').read())
 
 #print(matches)
 for match in matches:
